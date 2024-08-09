3028ebfecc7b9c5b06696a639540f99459fb9c10
anthony-thnitsolutions <anthony@kraftamine.in>
2024-08-09 14:12:57 +0530

    Initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,22 @@
+
+function Header(){
+
+  return(
+    <>
+    <div className="justify-between flex px-[2vw] py-[1vh]">
+      <a href="/"><h1 className="font-sans uppercase font-semibold text-lg text-slate-300">Anthony Prakash Rozario</h1></a>
+      <div>
+
+      </div>
+      <div>
+      <a href="http://" target="_blank" rel="noopener noreferrer">
+            <i className="pi pi-twitter"></i>
+          </a>
+      <a className="px-4 py-2 border-2 border-gray-400 text-gray-400 ">Resume </a>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Header
